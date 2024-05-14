d8968e70eb583dbcae5be509c63d2100fdf45cfb
StevenL119 <164972278+StevenL119@users.noreply.github.com>
2024-05-14 14:58:31 +0700

    Update db.py
    change the db tools from mysql to sqlite3 and created two seperate tables in the database storing cycing records like time taken, average speed, and such

--- db.py
+++ db.py
@@ -1,12 +1,5 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="admin",
-  password="111"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE Cycling")
-
+import sqlite3
+con = sqlite3.connect('cycling')
+cur = con.cursor()
+cur.execute("CREATE TABLE Record1(Time Taken, Start, End)")
+cur.execute("CREATE TABLE Record2(Distance Travelled, Average Speed, Highest Speed)")
